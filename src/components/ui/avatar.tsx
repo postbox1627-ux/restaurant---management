f55065f49e4e1af2f25466dd735e9ec6be448145
postbox1627-ux <postbox1627@gmail.com>
2026-04-11 19:56:50 +0530

    Update avatar.tsx

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,15 +1,32 @@
 export const Avatar = ({ children }: any) => {
   return (
-    <div style={{
-      width: "40px",
-      height: "40px",
-      borderRadius: "50%",
-      background: "#ccc",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center"
-    }}>
+    <div
+      style={{
+        width: "40px",
+        height: "40px",
+        borderRadius: "50%",
+        overflow: "hidden",
+        background: "#ccc",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       {children}
     </div>
   );
+};
+
+export const AvatarImage = ({ src }: any) => {
+  return (
+    <img
+      src={src}
+      alt="avatar"
+      style={{ width: "100%", height: "100%", objectFit: "cover" }}
+    />
+  );
+};
+
+export const AvatarFallback = ({ children }: any) => {
+  return <span>{children}</span>;
 };
