ba932b4d8c4a9998d3e29d396f486005644e458b
postbox1627-ux <postbox1627@gmail.com>
2026-04-11 20:11:59 +0530

    Update select.tsx

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,7 +1,19 @@
 export const Select = ({ children, ...props }: any) => {
-  return (
-    <select {...props} style={{ padding: "8px" }}>
-      {children}
-    </select>
-  );
+  return <select {...props}>{children}</select>;
+};
+
+export const SelectTrigger = ({ children }: any) => {
+  return <div>{children}</div>;
+};
+
+export const SelectValue = ({ children }: any) => {
+  return <span>{children}</span>;
+};
+
+export const SelectContent = ({ children }: any) => {
+  return <>{children}</>;
+};
+
+export const SelectItem = ({ children, value }: any) => {
+  return <option value={value}>{children}</option>;
 };
